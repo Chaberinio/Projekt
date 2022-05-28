8e56a4d8ff19bb981f885fe7a4978064dc00f476
MrSquirrel21 <91244402+Squirrel21git@users.noreply.github.com>
2022-05-28 22:47:17 +0200

    naprawienie linków do podstron

--- Projekt/routes/index.js
+++ Projekt/routes/index.js
@@ -7,4 +7,28 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/showGames', (req, res) => {
+    res.render('showGames', { title: "Lista gier" });
+});
+
+router.get('/showDev', (req, res) => {
+    res.render('showDevelopers', { title: "Lista deweloperów" });
+});
+
+router.get('/showGenre', (req, res) => {
+    res.render('showGenre', { title: "Lista gatunków" });
+});
+
+router.get('/addGame', (req, res) => {
+    res.render('addGame', { title: "Dodaj grê" });
+});
+
+router.get('/addDev', (req, res) => {
+    res.render('addDev', { title: "Dodaj dewelopera" });
+});
+
+router.get('/addGenre', (req, res) => {
+    res.render('addGenre', { title: "Dodaj gatunek" });
+});
+
 module.exports = router;
